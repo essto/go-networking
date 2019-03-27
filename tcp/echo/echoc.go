21c45fa1d3c095b26d2b0d5250b43c7f4b70eefa
Se Bastian <>
2019-03-27 12:34:33 +0100

    initial fork, add comments

--- tcp/echo/echoc.go
+++ tcp/echo/echoc.go
@@ -7,27 +7,28 @@ import (
 	"os"
 )
 
-// This program implements a simple echo client over tcp.
+// This program implements a simple echo client over tcp
 // It sends a text content to the server and displays
-// the response on the screen.
+// the response on the screen
 //
 // Usage: echoc -e <host-addr-endpoint> <text content>
 func main() {
 	var addr string
+	// set up default flag
 	flag.StringVar(&addr, "e", "localhost:4040", "service address endpoint")
 	flag.Parse()
 	text := flag.Arg(0)
 
-	// use ResolveTCPAddr to create address to connect to
+	// call ResolveTCPAddr to create address that is bound to this value (passed by flag "e")
 	raddr, err := net.ResolveTCPAddr("tcp", addr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// Use DialTCP to create a connection to the
-	// remote address. Note that there is no need
-	// to specify the local address.
+	// next calll DialTCP to create a connection to the remote address
+	// !!! there is no need to specify the local address
+	// return the conn of typ TCPconn
 	conn, err := net.DialTCP("tcp", nil, raddr)
 	if err != nil {
 		fmt.Println("failed to connect to server:", err)
@@ -36,6 +37,8 @@ func main() {
 	defer conn.Close()
 
 	// send text to server
+	// takes as value text we get from the command line
+	// text converts to slice of bytes an send it to the host
 	_, err = conn.Write([]byte(text))
 	if err != nil {
 		fmt.Println(err)
@@ -43,7 +46,9 @@ func main() {
 	}
 
 	// read response
+	// set up a buf of size
 	buf := make([]byte, 1024)
+	// n - bytes we read from the remote host
 	n, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("failed reading response:", err)
@@ -51,3 +56,8 @@ func main() {
 	}
 	fmt.Println(string(buf[:n]))
 }
+
+// go run echos.go "hello"
+
+// go run echos.go
+// hello
--- tcp/echo/echos.go
+++ tcp/echo/echos.go
@@ -15,30 +15,37 @@ import (
 // echos -e <host:address>
 func main() {
 	var addr string
+	// bind to port :4040 an all local interface
 	flag.StringVar(&addr, "e", ":4040", "service address endpoint")
 	flag.Parse()
 
-	// create local addr for socket
+	// create local address for socket
 	laddr, err := net.ResolveTCPAddr("tcp", addr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// announce service using ListenTCP
-	// which a TCPListener.
+	// #1 announce service using ListenTCP
+	// which a TCPListener
+	// this call create a listener for client
 	l, err := net.ListenTCP("tcp", laddr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	//http://rosettacode.org/wiki/Exceptions#Go
+	//http://rosettacode.org/wiki/Exceptions#Python
 	defer l.Close()
 	fmt.Println("listening at (tcp)", laddr.String())
 
-	// req/response loop
+	// #2 req/response loop
 	for {
-		// use TCPListener to block and wait for TCP
+		// AcceptTCP on that listener
+		// use TCPListener to block and waiting for request from the client
 		// connection request using AcceptTCP which creates a TCPConn
+		// we have conn value which we can use to subsequence interaction to the client
+		// conn is type TCPConn
 		conn, err := l.AcceptTCP()
 		if err != nil {
 			fmt.Println("failed to accept conn:", err)
@@ -47,32 +54,50 @@ func main() {
 		}
 		fmt.Println("connected to: ", conn.RemoteAddr())
 
+		// pass coonn to the new goroutine
+		// goroutine allows the listener loop to contiue and come backs to l.AcceptTCP()
+		// blok and waiting
+		// goroutine https://gobyexample.com/goroutines
+		// but the same time... #3
 		go handleConnection(conn)
 	}
 }
 
-// handleConnection reads request from connection
-// with conn.Read() then write response using
-// conn.Write().  Then the connection is closed.
+// #3 ... handleConnection will kick off
+// handleConnection reads request from connection with conn.Read()
+// then write response using conn.Write()
+// then the connection is closed
 func handleConnection(conn *net.TCPConn) {
+	// the first make sure when handleConnection is down
+	// this connection will close properly
 	defer conn.Close() // clean up when done
 
+	// set up a size buffer
 	buf := make([]byte, 1024)
 
+	// #4 use conn.Read to block and wait on values from the client
+	// read something form the client err or the n (number of bytes)
 	n, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// echo buffer
+	// #5 user conn.Write method send back to the client the content we read
+	// from 0 to n bytes
 	w, err := conn.Write(buf[:n])
 	if err != nil {
 		fmt.Println("failed to write to client:", err)
 		return
 	}
+	// silly check to make sure the number of bytes was written we read form the client
 	if w != n { // was all data sent
 		fmt.Println("warning: not all data sent to client")
 		return
 	}
 }
+
+// go run echos.go -h
+// go run echos.go
+
+// nc localhost 4040
